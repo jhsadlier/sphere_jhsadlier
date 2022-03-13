c157fa4016f572256688cca8e7199f6439054f15
Jessica <Jessica@DESKTOP-4KDM5PB>
2022-03-13 17:06:55 -0400

    Tmp save during fork

--- sphere.groovy
+++ sphere.groovy
@@ -2,8 +2,8 @@
 CSG sphere = new Sphere(25)// Spheres radius
 				.toCSG()// convert to CSG to display
 				
-CSG sphereHighRes = new Sphere(25,// Spheres radius
-						40,//elevation Divisions
+CSG sphereHighRes = new Sphere(55,// Spheres radius
+						60,//elevation Divisions
 						40)// vertical divisions
 						.toCSG()// convert to CSG to display
 sphere=sphere.movex(55)
